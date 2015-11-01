90a980fb34740e104e75087326fc4c1c1c2bcd23
joshqn <joshmkuehn@gmail.com>
2015-10-31 19:46:31 -0500

    Added a second VC
    AddItemViewController added

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController {
+class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate {
     var items:[ChecklistItem]
     
     required init?(coder aDecoder: NSCoder) {
@@ -55,6 +55,14 @@ class ChecklistViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "AddItem" {
+            let navigationController = segue.destinationViewController as! UINavigationController
+            let controller = navigationController.topViewController as! AddItemViewController
+            controller.delegate = self
+        }
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -106,16 +114,24 @@ class ChecklistViewController: UITableViewController {
         label.text = item.text
     }
     
-    @IBAction func addItem () {
+    //MARK: AddItemViewControllerDelegate
+    
+    func addItemViewControllerDidCancel(controller: AddItemViewController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func addItemViewController(controller: AddItemViewController, didFinishAddingitem item: ChecklistItem) {
+        
         let newRowIndex = items.count
-        let item = ChecklistItem()
-        item.text = "I am a new Row"
-        item.checked = true
+        
         items.append(item)
         
         let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
         let indexPaths = [indexPath]
         tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+
+        
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
 }
