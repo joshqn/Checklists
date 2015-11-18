98cd56650a2e2db19869d6e10c28df1c35d70676
joshqn <joshmkuehn@gmail.com>
2015-11-18 15:58:26 -0600

    Added LocalNotifications and UIDatePicker
    NSNotifications were enabled for the user so that they will be notified when a date is reached plus the uidatePicker was added

---
+++ Checklists/DataModel.swift
@@ -0,0 +1,9 @@
+//
+//  DataModel.swift
+//  Checklists
+//
+//  Created by Joshua Kuehn on 11/7/15.
+//  Copyright © 2015 Joshua Kuehn. All rights reserved.
+//
+
+import Foundation
---
+++ Checklists/IconPickerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  IconPickerViewController.swift
+//  Checklists
+//
+//  Created by Joshua Kuehn on 11/12/15.
+//  Copyright © 2015 Joshua Kuehn. All rights reserved.
+//
+
+import Foundation
